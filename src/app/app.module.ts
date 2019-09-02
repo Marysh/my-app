81275ca910a24f0f61c9bd9cbf043bf2e55a9632
Marysh <35464372+Marysh@users.noreply.github.com>
2019-09-02 19:20:36 +0300

    todo-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RequestComponent } from './request/request.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TodoComponent } from './todo/todo.component';
+import { TableComponent } from './todo/table/table.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+const appRoutes: Routes = [
+  // { path: '', component: AppComponent},
+  {
+    path: 'request',
+    component: RequestComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent,
+  }
+  // { path: '',
+  //   redirectTo: '/',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestComponent,
+    PageNotFoundComponent,
+    TodoComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    AppRoutingModule,
+    NgbModule,
+    NgbPaginationModule, NgbAlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
